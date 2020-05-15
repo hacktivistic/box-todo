4de4d4a2a354045ddb236eed0a27e6ff651975f2
Robin Panta <hacktivistic@gmail.com>
2020-05-16 00:53:18 +0530

    feat: create routes

---
+++ routes/home.js
@@ -0,0 +1,26 @@
+/**
+ * Home Route
+ */
+
+const Router = require('express').Router()
+
+Router.get('/', (req, res) => {
+    res.end('Todo Public API v1.0')
+})
+
+
+Router.get('/todo', (req, res) => {
+    
+    const todos = req.box.Todo
+    res.json(todos.retrieve())
+
+})
+
+Router.post('/todo', (req, res) => {
+    
+    res.json(req.box.Todo.create(req.body.task))
+
+})
+
+
+module.exports = Router
